cbbe6ded698b77e7f1abf5611bc63a982f948259
RavenHrafnagud <hrafnfreistudrr@gmail.com>
2026-03-18 19:15:35 -0500

    se arreglo un problema con el deploy al momento de realizar el pase a produccion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Required for GitHub Pages project sites.
+  base: '/crediflex/',
   plugins: [react()],
 })
